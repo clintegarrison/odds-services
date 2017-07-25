d727f64c91bf82a5c466e2982f02189337c7ef87
cxg5940 <clint_garrison@homedepot.com>
2017-07-25 13:47:33 -0400

    added CORS support

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const fetchUrl = require('fetch').fetchUrl
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/getMoneyLines', function(req, res){
   console.log('gettingMoneyLines')
   var moneyLineMatchups = []
